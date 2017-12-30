602ab81ea5714611183b481f2ed0f04e9d08ebf5
machavez84 <machavez@protonmail.com>
2017-12-30 21:24:17 +0000

    updated

--- openvpnConnect.sh
+++ openvpnConnect.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Openpvn connect script
+#Author: Manuel A. Chavez M.
 
 #Resolv.conf restore function
 clean()
@@ -7,42 +8,75 @@ clean()
 echo "Restoring resolv.conf and deleting temporal files..."
 mv /etc/resolv.conf.old /etc/resolv.conf
 rm $OVPN.temp
+sleep 5s
+echo "Done"
+}
+
+Usage()
+{
+echo "openvpnConnect <host> <port> <.opvn absolute path>" 
+}
+
+#Hostname Resolv function
+ResolvHost()
+{
+echo "Resolving $HOST IP..."
+IP=$(nslookup $HOST | grep Address | tail -1 | cut -c 10-25)
+sleep 2s
+echo "The ip is: $IP"
+}
+
+
+#Temporary .ovpn file creation
+CreateConf()
+{
+echo "Creating configuration file..."
+sed '4s/.*/remote '$IP' '$PORT' /' $OVPN > $OVPN.temp
 sleep 2s
 echo "Done"
 }
 
-#Initialising Varibles
-HOST=$1
-PORT=$2
-OVPN=$3.ovpn
 
+#Function to modify resolv.conf file
+ModifyResolvConf()
+{
+echo "Modificando resolv.conf"
+mv /etc/resolv.conf /etc/resolv.conf.old
+echo nameserver 192.168.0.1 > /etc/resolv.conf
+sleep 2s
+echo "cat /etc/resolv.conf"
+echo "Done"
+}
+
+Connect()
+{
+echo "Conectando...(CTRL-C para desconectar de la vpn)"
+sleep 2s
+trap "clean" 2
+openvpn --config $OVPN.temp 
+}
+
+
+Main()
+{
 #test
 echo $HOST
 echo $PORT
 echo $OVPN
 
-echo "Resolving $HOST IP..."
-IP=$(nslookup $HOST | grep Address | tail -1 | cut -c 10-25)
-sleep 2s
-echo "The ip is: $IP"
+ResolvHost
+CreateConf
+ModifyResolvConf
+Connect
+}
 
-echo "Creating configuration file..."
-sed '4s/.*/remote '$IP' '$PORT' /' $OVPN > $OVPN.temp
-sleep 2s
-echo "Done"
 
-echo "Modificando resolv.conf"
-mv /etc/resolv.conf /etc/resolv.conf.old
-echo nameserver 192.168.1.1 > /etc/resolv.conf
-sleep 2s
-echo "cat /etc/resolv.conf"
-echo "Done"
+#Initialising Varibles
+HOST=$1
+PORT=$2
+OVPN=$3
 
-echo "Conectando...(CTRL-C para desconectar de la vpn)"
-sleep 2s
-trap "clean" 2
-openvpn --config $OVPN.temp 
-
+Main
 exit 0
 
 
